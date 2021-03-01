13728f263c1225f5c1b530c206f6b78aade7f3a9
Ravi Bullock <ravioliasb@gmail.com>
2021-02-28 19:52:54 -0500

    Added Script and Function

---
+++ analysis/scripts/project_functions.py
@@ -0,0 +1,21 @@
+import pandas as pd
+def load_and_process(url_or_path_to_csv_file):
+    
+    df = (
+        pd.read_csv(url_or_path_to_csv_file)
+        .rename(columns={"age":"Age"})
+        .rename(columns={"sex":"Sex"})
+        .rename(columns={"bmi":"BMI"})
+        .rename(columns={"children":"Number of Children"})
+        .rename(columns={"smoker":"Smoker"})
+        .rename(columns={"region":"Place of Residence"})
+        .rename(columns={"charges":"Medical Insurance Charges"})
+
+
+
+        
+    )
+    
+    return df
+    
+    
